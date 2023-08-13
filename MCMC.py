3c96b8209272619c72c28b32b0d5f31ded564a4c
Michael Liu <84533632+michaelmml@users.noreply.github.com>
2023-08-13 21:57:48 +0100

    Update MCMC.py

--- MCMC.py
+++ MCMC.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import yfinance as yf
 from scipy.stats import norm
-from scikit-learn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression
 
 ###############
 
